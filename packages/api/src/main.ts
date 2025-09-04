24feb01e6b048ac5597e75afc6f11d5d41594762
Arnoldsteve <stevearnold9e@gmail.com>
2025-09-04 15:25:27 +0300

    allow origins

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3002'],
+    origin: ['http://localhost:3000', 'http://localhost:3002', 'https://artisan-base-storefront.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization, x-tenant-id',
     credentials: true,
