63a333430100d6ecc433938b74e03132dbfeda55
Arnoldsteve <stevearnold9e@gmail.com>
2025-09-19 17:11:26 +0300

    minor chnages

--- packages/dashboard/eslint.config.mjs
+++ packages/dashboard/eslint.config.mjs
@@ -11,11 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals"),
-  // { 
-  //     rules: {
-  //     "react-hooks/exhaustive-deps": "off"
-  //   },
-  // },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ]
 
 export default eslintConfig
--- packages/dashboard/next.config.ts
+++ packages/dashboard/next.config.ts
@@ -3,21 +3,33 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'pqdkftosfeulneizqvmi.supabase.co', // Your specific Supabase project hostname
-        port: '',
-        pathname: '/storage/v1/object/public/**', // Allow any image from public storage
+       {
+        protocol: "https",
+        hostname: "pqdkftosfeulneizqvmi.supabase.co", 
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com", 
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-  
+
   webpack: (config) => {
     config.cache = false;
     return config;
