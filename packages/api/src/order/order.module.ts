c8a56aac04a28f6bfd8c5da9079c25f6f6fe0859
Arnoldsteve <stevearnold9e@gmail.com>
2026-02-23 18:20:47 +0300

    order module minor changes

--- packages/api/src/order/order.module.ts
+++ packages/api/src/order/order.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
 import { OrderRepository } from './repositories/order.repository';
+import { ProductModule } from '@/product/product.module';
+import { UserModule } from '@/user/user.module';
 
 @Module({
-  imports: [],
+   imports: [
+    ProductModule, // Gives access to ProductRepository
+    UserModule,    // Gives access to UserRepository
+  ],
   controllers: [OrderController],
   providers: [OrderRepository, OrderService],
   exports: [OrderService, OrderRepository],
